e1b6b0292e2bf8979da86368781055fa036dddfb
Jillberth Estillore <ejillberth@gmail.com>
2024-10-15 15:33:27 +0200

    Fix page title

--- resources/views/meeting.blade.php
+++ resources/views/meeting.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>QR Code Display</title>
+    <title>Meeting</title>
     <style>
         body {
             margin: 0;
@@ -43,6 +43,7 @@
             font-size: 16px;
             cursor: pointer;
             text-decoration: none;
+            font-family: 'Trebuchet MS';
         }
 
         .button:hover {
